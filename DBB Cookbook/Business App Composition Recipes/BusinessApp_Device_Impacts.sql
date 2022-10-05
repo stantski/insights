d4011a7623315c3d1c4d508cb926ab2378f72cf4
nmoisan42 <nicholas.moisan@device42.com>
2022-10-05 16:28:55 -0400

    Device Impacts column cleanup

--- DBB Cookbook/Business App Composition Recipes/BusinessApp_Device_Impacts.sql
+++ DBB Cookbook/Business App Composition Recipes/BusinessApp_Device_Impacts.sql
@@ -2,17 +2,21 @@
  * Discover all business apps that are impacted by a specific set of infrastructure devices and the relationships between them.
  */
 SELECT
-    object_name
+    businessapplication_fk
+    ,business_app_name
     ,device_fk
     ,impacted_object
     ,device_host
     ,device_subtype
     ,impact_reason
     ,relationship_type
-    ,businessapplication_fk
-    ,business_app_name
-    ,port
+    ,relationship_url
+    ,object_name                        AS related_object_name
+    ,port                               AS related_object_port
+    ,related_object_url
     ,component_type
+    ,business_app_url
+    ,business_app_diagram
     ,business_app_tech_owner
     ,business_app_owner
     ,business_app_service_level
@@ -23,13 +27,10 @@ SELECT
     ,business_app_contains_pii
     ,business_app_internet_accessible
     ,business_app_notes
-    ,related_object_url
-    ,relationship_url
-    ,business_app_url
-    ,business_app_diagram
 FROM
     view_dbb_adm_businessapp_composition_v2
 WHERE 
     object_name IN('<your-device-name-here>')
+    OR impacted_object IN('<your-device-name-here>')
 ORDER BY
     business_app_name ;
